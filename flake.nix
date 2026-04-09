d07b6915bedcd01572704aae7b6d1dc605fe23f7
dmitriy sokolov <codemitry@gmail.com>
2026-04-09 16:27:33 +0300

    cangjie runtime fixes; more scripts

--- flake.nix
+++ flake.nix
@@ -4,16 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs-old.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-25.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-old, nixpkgs-25, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixpkgs-old, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
         pkgs = import nixpkgs { inherit system; };
         pkgs-old = import nixpkgs-old { inherit system; };
-        pkgs-25 = import nixpkgs-25 { inherit system; };
+
+        clang15 = pkgs-old.llvmPackages_15.clang;
 
         arch-cmd = pkgs.writeShellScriptBin "arch" ''
             uname -m
@@ -51,15 +51,21 @@
                 libedit
                 swig
 
+                jdk17
+
                 # for $(arch) in envsetup.sh
                 arch-cmd
             ];
+
+            CC = "${clang15}/bin/clang";
+            CXX = "${clang15}/bin/clang++";
             
             CMAKE_CXX_COMPILER_LAUNCHER="${pkgs.ccache}/bin/ccache";
             CMAKE_C_COMPILER_LAUNCHER="${pkgs.ccache}/bin/ccache";
             # use correct glibc
             LIBRARY_PATH = "${sysroot}/lib";
-
+            GNU_LD = "${sysroot}/lib/ld-linux-x86-64.so.2";
+            GNU_TOOLCHAIN = "${sysroot}";
 
             shellHook = ''
                 echo "~~ cangjie dev ~~"
